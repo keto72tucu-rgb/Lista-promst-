c26e731bad8a99f6ef072e40e084f52bc10af295
keto72tucu-rgb <keto72tucu@gmail.com>
2026-09-23 19:49:56 -0300

    Agregar módulo de la app

---
+++ app/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "ar.com.jorgeabella.japrompts"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "ar.com.jorgeabella.japrompts"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+}
